efd33ea48357e11d2e71c65acfab8303a3c8a29a
Lazar Stojanović <lakisd98@gmail.com>
2026-05-22 10:26:11 +0200

    Add: Todo post functionality and request.http tests

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Controller('todos')
 export class TodoController {
   @Get()
   getTodos() {
     return [];
+  }
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDto) {
+    return createTodoDto;
   }
 }
